4ee6dc7bb12bbb65d7ad977a6501021865d0d377
im-ajin <ajin17117@gmail.com>
2024-01-25 22:07:43 -0800

    create sign up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Ajin's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is not very good because its not good so you cant understand
+            this thinks in this time so try next time to understand this
+            sentence.
+          </p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div className="">
+                <Label value="Your username" />
+                <TextInput type="text" placeholder="Username"
+                id="username" />
+            </div>
+            <div className="">
+                <Label value="Your email" />
+                <TextInput type="text" placeholder="Email"
+                id="email" />
+            </div>
+            <div className="">
+                <Label value="Your password" />
+                <TextInput type="text" placeholder="Password"
+                id="password" />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type="submit">SignUp</Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to='/sign-in' className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
