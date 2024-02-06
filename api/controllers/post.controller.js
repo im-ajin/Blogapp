716d0df1205217f21eb100a696fed1b83f822485
im-ajin <ajin17117@gmail.com>
2024-02-06 08:12:53 -0800

    add update post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -27,20 +27,24 @@ export const getposts = async (req, res, next) => {
        const startIndex = parseInt(req.query.startIndex || 0);
        const limit = parseInt(req.query.limit) || 9;
        const sortDirection = req.query.order === 'asc' ? 1 : -1;
-
-       const posts = await Post.find({
-        ...(req.query.userId && { userId: req.query.userId}),
-        ...(req.query.category && { category: req.query.category}),
-        ...(req.query.slug && { category: req.query.slug }),
-        ...(req.query.postId &&{ _id: req.query.postId }),
-        ...(req.query.searchTerm && {
-             $or: [
-                { title: { $regex: req.query.searchTerm, $option: 'i'}},
-                { content: { $regex: req.query.searchTerm, $options: 'i'}},
-             ],
-        }),
-
-    }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
+       
+       const objj = {
+        
+            ...(req.query.userId && { userId: req.query.userId}),
+            ...(req.query.category && { category: req.query.category}),
+            ...(req.query.slug && { category: req.query.slug }),
+            ...(req.query.postId &&{ _id: req.query.postId }),
+            ...(req.query.searchTerm && {
+                 $or: [
+                    { title: { $regex: req.query.searchTerm, $option: 'i'}},
+                    { content: { $regex: req.query.searchTerm, $options: 'i'}},
+                 ],
+            }),
+    
+        
+       }
+       
+       const posts = await Post.find(objj).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
 
     const totalPosts = await Post.countDocuments();
     const now = new Date();
@@ -73,6 +77,27 @@ export const deletepost = async (req, res, next) => {
         res.status(200).json('The post has been deleted');
     }
     catch(error){
+        next(error)
+    }
+}
+
+export const updatepost = async (req, res, next) => {
+    if(!req.user.isAdmin || req.user.id !== req.params.userId){
+        return next(errorHandler(403, 'You are not allowed to update this post'))
+    }
+    try{
+        const updatePost = await Post.findByIdAndUpdate(
+            req.params.postId,{
+                $set: {
+                    title: req.body.title,
+                    content: req.body.content,
+                    category: req.body.category,
+                    image: req.body.image
+                }
+            },{ new: true}
+        )
+        res.status(200).json(updatePost)
+    }catch(error){
         next(error)
     }
 }
