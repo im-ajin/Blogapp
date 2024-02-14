b2a5d7c7075835de3242da65b24a43287ca8f34f
im-ajin <ajin17117@gmail.com>
2024-02-14 06:02:04 -0800

    add commentsection to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import { Sidebar } from 'flowbite-react'
-import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
+import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
 import { signoutSuccess } from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
@@ -56,11 +56,20 @@ const DashSidebar = () => {
           )} 
 
           {currentUser.isAdmin && (
+            <>
             <Link to='/dashboard?tab=users'>
             <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
               Users
             </Sidebar.Item>
           </Link>
+
+          <Link to='/dashboard?tab=comments'>
+            <Sidebar.Item active={tab === 'users'} icon={HiAnnotation} as='div'>
+              Comments
+            </Sidebar.Item>
+          </Link>
+
+          </>
           )}
 
           
