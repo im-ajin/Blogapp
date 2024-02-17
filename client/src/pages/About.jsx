94667b45198249b9443b42e0fcf43a0d565a99a0
im-ajin <ajin17117@gmail.com>
2024-02-16 22:25:02 -0800

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>About Sahand's Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, libero aspernatur. Accusantium maiores blanditiis reprehenderit quod exercitationem soluta harum earum? Reiciendis officiis quae ullam nemo molestiae suscipit, quasi deleniti odit!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam enim earum dolores distinctio vero eos accusantium nobis libero quis rem sunt, quod ducimus sed facere quia dolorem totam a voluptates!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores veniam quaerat praesentium harum officia quam sed non, ducimus corporis placeat illum nemo laudantium fugiat quis ipsa commodi doloribus consequatur vel.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex flex-col justify-center items-center gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime quasi fuga dolore porro, perspiciatis voluptates necessitatibus tempore odit error eligendi? Saepe corrupti nesciunt totam. Voluptatum deleniti ipsa sequi assumenda beatae!</p>
+      <CallToAction />
+    </div>
   )
 }
 
