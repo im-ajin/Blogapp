0f461049144c5ed466a139d7356150fb5ac8bd51
im-ajin <ajin17117@gmail.com>
2024-07-15 23:04:09 +0530

    changed about.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,10 +5,9 @@ const About = () => {
     <div className='min-h-screen flex items-center justify-center'>
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
-          <h1 className='text-3xl font font-semibold text-center my-7'>About Sahand's Blog</h1>
+          <h1 className='text-3xl font font-semibold text-center my-7'>Im Ajin</h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
-            <p>"Welcome to My Blog, where words dance with imagination and ideas take flight. Dive into a world where curiosity meets creativity, where every post is a journey into the realms of knowledge, inspiration, and beyond. From insightful articles to captivating stories, My Blog is your oasis of intellect and wonder in the vast digital landscape. Join us as we explore the beauty of language, the depths of human experience, and the endless possibilities of the mind. Come, wander with us, and let your imagination soar."</p>
-            <p>"At My Blog, we believe that every word has the power to spark a revolution of thought, ignite a passion, or simply bring a smile to someone's face. Our corner of the internet is a sanctuary for the curious, the dreamers, and the seekers of inspiration. Through our diverse array of articles, essays, and musings, we aim to stir your intellect, provoke your emotions, and leave you with a newfound appreciation for the beauty of language and the boundless potential of human imagination. Join our community of like-minded individuals as we embark on a journey of discovery, reflection, and connection. Welcome to My Blog, where words come alive."</p>
+            <p>Do what you want</p>
           </div>
         </div>
       </div>
